83770b0db9fc6453d572dbba878b65ea2258c223
q4artz <116222352+q4artz@users.noreply.github.com>
2022-11-09 19:34:36 +0800

    Add files via upload

---
+++ tac.c
@@ -0,0 +1,51 @@
+/*
+Need what?
+->> player,winner,loser,computer
+->> board,turns(do while)
+->> 
+*/
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdbool.h>
+void Board(){
+    int board[3][3];
+    printf("  %c |  %c | %c ",board[0][0],board[0][1],board[0][2]);
+    printf("\n ---|----|---|\n");
+    printf("  %c | %c  | %c ",board[1][0],board[1][1],board[1][2]);
+    printf("\n ---|----|---|\n");
+    printf("  %c | %c  | %c  ",board[2][0],board[2][1],board[2][2]);
+    printf("\n");
+}
+void ResetBoard(unsigned board[3][3]){
+    size_t board[3][3];
+    for(size_t i=0; i<3; i++){
+        for(size_t j=0; j<3; j++){
+            puts(" ");
+        }
+    }
+}
+int StartGame(bool(*GameCondition)(int));
+int checkFreeSpaces();
+int UserInput(int *const row,int *const column);
+int ComInput(int const *row,int const *column);
+int main(){
+
+    // 0 == win , 1 == Lose , 2 == Draw ;
+    int GameStatus;
+
+    if(GameStatus == 0){
+        puts("You win!");
+        return 0;
+    }
+    else{
+        if(GameStatus == 1){
+           ResetBoard();
+           Board();
+        }
+    }
+
+   
+}
