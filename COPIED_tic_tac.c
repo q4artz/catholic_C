a8b3dc76b4916b22ce95fbad4c05bb5e44af9f00
q4artz <116222352+q4artz@users.noreply.github.com>
2022-11-22 17:24:22 +0800

    Add files via upload

--- COPIED_tic_tac.c
+++ COPIED_tic_tac.c
@@ -1,3 +1,5 @@
+// Code Copied from bro code; Used for reference 
+// for my tac.c mini project to learn about pointers
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -47,9 +49,9 @@ void resetBoard(){
 }
 void printBoard(){
     printf("  %c |  %c | %c ",board[0][0],board[0][1],board[0][2]);
-    printf("\n---|---|---|\n");
+    printf("\n ---|----|---|\n");
     printf("  %c | %c  | %c ",board[1][0],board[1][1],board[1][2]);
-    printf("\n---|---|---|\n");
+    printf("\n ---|----|---|\n");
     printf("  %c | %c  | %c  ",board[2][0],board[2][1],board[2][2]);
     printf("\n");
 }
