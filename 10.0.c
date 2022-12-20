9739a219fdd5e823338dc66eb5ddf50e47803a4f
q4artz <116222352+q4artz@users.noreply.github.com>
2022-12-20 14:35:02 +0800

    Add files via upload

---
+++ 10.0.c
@@ -0,0 +1,64 @@
+// Chapter 10 C structures , Unions, Bit Manipulation, Enumerations
+
+// 10.2 Structure Defitnition
+/*
+Struct are related variables under one name
+((may contain same or different variables))
+
+example 
+
+struct employees{
+    char firstName[20];
+    char lastName[20];
+    unsigned int age;
+    char gender;
+    double salaryhours;
+};
+*/
+
+/*
+Struct cannot be decleared in another struct unless it is a pointer
+
+example
+
+struct company{
+    struct employees teamLeader; // error
+    struct employees *PtrTeamLeader; // pointer no error
+}
+*/
+
+//10.2.2 Defining Variables of structure types
+/*
+
+// ((we can declear a variable to the the type of the struct))
+// currently idk what it is for but i plan to find out soon
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct card{
+    char *face;
+    char *suit;
+}aCard,deck[52],*cardPtr;
+*/
+
+// 10.3 Initailiing Structures
+
+#include <stdio.h>
+#include <stdlib.h>
+struct card{
+    // initialize what each struct should have inside
+    char *face;
+    char *suit;
+}aCard,deck[52],*cardPtr; // initailize what variable should hold the struct
+
+struct card aCard = {
+    // initializing the struct 
+    // the face from *face now is Three in aCard
+    "Three",
+    // the *suit from *suit now is Hearts in aCard
+    "Hearts"
+};
+int main(){
+    printf("%s\n",aCard.suit);
+}
