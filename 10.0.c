f6aa670a1c70bedc77df462c74b57194395cc177
q4artz <116222352+q4artz@users.noreply.github.com>
2023-01-05 05:57:37 +0800

    Add files via upload

--- 10.0.c
+++ 10.0.c
@@ -4,9 +4,7 @@
 /*
 Struct are related variables under one name
 ((may contain same or different variables))
-
 example 
-
 struct employees{
     char firstName[20];
     char lastName[20];
@@ -18,9 +16,7 @@ struct employees{
 
 /*
 Struct cannot be decleared in another struct unless it is a pointer
-
 example
-
 struct company{
     struct employees teamLeader; // error
     struct employees *PtrTeamLeader; // pointer no error
@@ -29,13 +25,10 @@ struct company{
 
 //10.2.2 Defining Variables of structure types
 /*
-
 // ((we can declear a variable to the the type of the struct))
 // currently idk what it is for but i plan to find out soon
-
 #include <stdio.h>
 #include <stdlib.h>
-
 struct card{
     char *face;
     char *suit;
@@ -43,7 +36,7 @@ struct card{
 */
 
 // 10.3 Initailiing Structures
-
+/*
 #include <stdio.h>
 #include <stdlib.h>
 struct card{
@@ -61,4 +54,32 @@ struct card aCard = {
 };
 int main(){
     printf("%s\n",aCard.suit);
+}
+*/
+
+// 10.4 Accessing Structure Members with . and -> 
+/*
+(.) is the structure number operator
+    - access struct member via struct variable name
+    - like printf("%s\n",aCard.suit);
+(->) is the structure pointer operator/ arrow operator (used this in js alot)
+    - pointer 
+*/
+
+#include <stdio.h>
+struct card{
+    char *face;
+    char *suit;
+};
+
+int main(){
+    struct card aCard;
+    
+    aCard.face="Ace";
+    aCard.suit="Spades";
+
+    struct card *cardPtr = &aCard;
+    printf("%s%s%s\n%s%s%s\n%s%s%s\n",
+    aCard.face,"of",aCard.suit,cardPtr->face,"of",cardPtr->suit,
+    (*cardPtr).face,"of",(*cardPtr).suit);
 }
