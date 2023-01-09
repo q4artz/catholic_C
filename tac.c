eba383e38076d11665ec3ac22482a786520d2e73
q4artz <116222352+q4artz@users.noreply.github.com>
2023-01-09 18:23:12 +0800

    Add files via upload

--- tac.c
+++ tac.c
@@ -1,69 +1,65 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <time.h>
 #include <stdbool.h>
 #include <string.h>
 #define BoardColumn 3
 #define BoardRow 3
-// affected by ResetBoard() , CheckFreeSpaces() ,  UserInput(), ComInput();
+#define PLAYER 'X'
+#define COMPUTER 'O'
+// Runs first - showcase an empty board
 void ShowcaseBoard(){
     int ShowcaseBoard[BoardRow][BoardColumn];
     printf("    |   |   |");
-     printf("\n");
+    printf("\n");
     printf("    |   |   |");
-     printf("\n");
+    printf("\n");
     printf("    |   |   |");
     printf("\n");
 }
-void Board(int BoardChange){
-    int MainBoard[BoardRow][BoardColumn];
-    printf("  %c | %c  | %c  |",MainBoard[0][0],MainBoard[0][1],MainBoard[0][2]);
-    printf("\n -------------\n");
-    printf("  %c | %c  | %c  |",MainBoard[1][0],MainBoard[1][1],MainBoard[1][2]);
-    printf("\n -------------\n");
-    printf("  %c | %c  | %c  |",MainBoard[2][0],MainBoard[2][1],MainBoard[2][2]);
-    printf("\n");
+void Board(int BoardChange,int *PlayerMove,int *ComputerMove){
+    char MainBoard[4][4];
+    //*PlayerMove = PLAYER;
+    //*PlayerMove = MainBoard[4][4];
+    //printf("  %c | %c  | %c  |",MainBoard[0][0],MainBoard[0][1],MainBoard[0][2]);
+    //printf("\n -------------\n");
+    //printf("  %c | %c  | %c  |",MainBoard[1][0],MainBoard[1][1],MainBoard[1][2]);
+    //printf("\n -------------\n");
+    //printf("  %c | %c  | %c  |",MainBoard[2][0],MainBoard[2][1],MainBoard[2][2]);
+    //printf("\n");
 }
-// get data from UserInput() and ComInput() and sent to Board();
-void PtrBoard(int *PLayerMove){
-    int row,col;
-    int board[BoardRow][BoardColumn];
-    const char PLAYER = 'X';
-}
-// runs first ; reset Board();
+// runs second - reset the board in void Board();
 int ResetBoard(){
-    void Board(int BoardChange);
+    void Board(int BoardChange,int *PlayerMove,int *ComputerMove);
     char wboard[BoardRow][BoardColumn];
-    for(int RowCount=0; RowCount<3; RowCount++){
-        for(int ColumnCount=0; ColumnCount<3; ColumnCount++){
+    for(int RowCount=0; RowCount<2; RowCount++){
+        for(int ColumnCount=0; ColumnCount<2; ColumnCount++){
          //wboard[RowCount][ColumnCount] = ' ';
-         Board(wboard[RowCount][ColumnCount] = ' ');
+         Board(wboard[RowCount][ColumnCount] = ' ',0,0);
         }
     }
     return 0;
 }
 // affected by GameCondition
 int StartGame(bool(*GameCondition)(int));
-//affected by ResetBoard() , Board();
-int checkFreeSpaces(bool(*BoardCondition)(int)){
-    int Board;
-}
 
 // get data from main() ,the data from main() pass to UserInput(), 
 // UserInput() pass to int PtrBoard();
-void UserInput(int *const PtrPlayerRowInput,int *const PtrPlayerColInput){
-    void PtrBoard(int *PlayerMoveRow);
-    void resetBoard();
+void UserInput(){
+   void Board(int BoardChange,int *PlayerMove,int *ComputerMove);
     // pass 2 set of data from UserInput() to PassBoard()
     int Passboard[3][3];
-    Passboard[*PtrPlayerRowInput][*PtrPlayerColInput] = ResetBoard();
-    
-   if(Passboard[*PtrPlayerRowInput][*PtrPlayerColInput] == ' '){
-        puts("Player Input Error");
-   }
-   PtrBoard(&Passboard[*PtrPlayerRowInput][*PtrPlayerColInput]);
+    int PtrPlayerRowInput,PtrPlayerColInput;
+    printf("Please enter a row: ");
+    scanf("%d",&PtrPlayerRowInput);
+    PtrPlayerRowInput--;
+    printf("Please enter a  column ");
+    scanf("%d",&PtrPlayerColInput);
+    PtrPlayerColInput--;
+    printf("Player has inputted X on (%d,%d)\n",PtrPlayerRowInput,PtrPlayerColInput);
+    Passboard[PtrPlayerRowInput][PtrPlayerColInput] = PLAYER;
+   
+   Board(0,&Passboard[PtrPlayerRowInput][PtrPlayerColInput],0);
 }
 int ComInput(int const *ComRowGenerate,int const *ComColumnGenerate);
 int main(int argc, char **argv){
@@ -71,20 +67,13 @@ int main(int argc, char **argv){
     // 0 == win , 1 == Lose , 2 == Draw ;
     int GameStatus = 1;
     int UserInputRowMain,UserInputColMain;
-    ShowcaseBoard();
-    ResetBoard();
+   ShowcaseBoard();
+   ResetBoard();
 
     while(GameStatus != 0){
-    printf("Please enter a row: ");
-    scanf("%d",&UserInputRowMain);
-    UserInputRowMain--;
-    printf("Please enter a  column ");
-    scanf("%d",&UserInputColMain);
-    UserInputColMain--;
 
-    UserInput(&UserInputRowMain,&UserInputColMain);
+    UserInput();
     }
-
     puts("You win!");
     return 0;
 }
