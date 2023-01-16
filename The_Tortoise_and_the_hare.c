12e3fe73235f508c2811c9c149ffbe377f90c98a
q4artz <116222352+q4artz@users.noreply.github.com>
2023-01-16 16:43:18 +0800

    Add files via upload

---
+++ The_Tortoise_and_the_hare.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define track 70
+
+int Hare(int *HarePosition);
+int Tortoise(int *TortoisePosition);
+int GameCondition(int *HarePosition,int *TortoisePosition);
+int timer();
+
+int main(int argc,char *argv[]){
+    int TortoisePosition=1,HarePosition=1,clock=1,Condition=0;
+    srand(time(NULL));
+
+    puts("Race Start!!\n");
+
+   do{
+        clock = clock + timer();
+        printf("\nTime is now %d !!\n",clock);
+
+        HarePosition = Hare(&HarePosition);
+        TortoisePosition = Tortoise(&TortoisePosition);
+
+        Condition = GameCondition(&HarePosition,&TortoisePosition);
+    }while(Condition == 0);
+
+   if(Condition == 1)
+        puts("Hare won");
+   if(Condition == 2)
+    puts("Tortoise won");
+    
+return 0;
+}
+
+int Hare(int *HarePosition){
+    int Haremove =0;
+    int hare = *HarePosition;
+    Haremove = rand() % 10+1;
+   if (Haremove >= 3 && Haremove <= 4){
+       hare += 9;
+       puts("Hare Hopped 9 squares!!!");
+   }
+   else if(Haremove == 5){
+       hare -= 12;
+       puts("Hare Sliped 12 squares!!");
+   }
+   else if( Haremove >= 6 && Haremove <= 8){
+       hare += 1;
+       puts("Hare Hopped 1 squares!!");
+   }
+   else if(Haremove >= 9 && Haremove >= 10){
+       hare -= 2;
+       puts("Hare Sliped 2 squares!!");
+   }
+   else 
+       puts("hare Slept?????");
+    if(hare < 0)
+        hare =0;
+   printf("Hare is now at %d!\n",hare);
+    return hare;
+};
+int Tortoise(int *TortoisePosition){
+    int tortoise = *TortoisePosition;
+    int TortoiseMove = 0;
+    enum GameCondition;
+
+    TortoiseMove = rand() % 10+1;
+    if (TortoiseMove >= 1 && TortoiseMove <= 5){
+        tortoise += 3;
+        puts("Tortoise plod 3 squares!!");
+    }
+    else if(TortoiseMove >= 6 && TortoiseMove <= 7){
+        tortoise -= 6;
+        puts("Tortoise Sliped 6 squares!!");
+    }
+    printf("Tortoise is now at %d!\n",tortoise); 
+    if(tortoise < 0)
+        tortoise = 0;
+    return tortoise;
+};
+int GameCondition(int *HarePosition,int *TortoisePosition){
+    // 0 == ongoing ; 1 == Hare ; 2 == Tortoise
+    int GameStatus =0;
+    int hare = *HarePosition,tortoise = *TortoisePosition;
+     if(hare > 70){
+        return 1;
+     }
+     else if(tortoise > 70){
+        return 2;
+     }
+     else return 0;
+};
+int timer(){
+    int timer =0;
+    timer++;
+    return timer;
+};
