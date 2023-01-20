972453be19221e70cdbcb91c84ab1931ececfd06
q4artz <116222352+q4artz@users.noreply.github.com>
2023-01-20 20:13:40 +0800

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include<string.h>
+int main(){
+    /*
+    not so diff from unix file sys
+    r   -read
+    w   -write (overwriting)
+    a   -add
+    r+  - (open existing file) read and write (OW off)
+    w+  - (create new file ) read and write (OW on)
+    a+  - (open/create file) read and write (OW off (append new data))
+    */
+    FILE *FilePointer  = fopen("test.txt","r");
+    fprintf(FilePointer,"hel");
+    fputc('a',FilePointer);
+    fputs("\nthis is the puts line\n",FilePointer);
+
+    char PrintFile = fgetc(FilePointer);
+    while (PrintFile != EOF){
+        PrintFile = fgetc(FilePointer);
+        printf("%c", PrintFile);
+
+    };
+    fclose(FilePointer);
+}
